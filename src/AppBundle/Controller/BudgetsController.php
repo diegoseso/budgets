3a99cab233d180c612e7fe383cf831d605fa6da4
Diego José Sosa Díaz <diego.venezuela@gmail.com>
2017-05-10 11:52:50 +0200

    First approach for a crud api

--- src/AppBundle/Controller/BudgetsController.php
+++ src/AppBundle/Controller/BudgetsController.php
@@ -2,35 +2,80 @@
 
 namespace AppBundle\Controller;
 
-class BudgetsController
+use AppBundle\Document\Product;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ProductController extends Controller
 {
-    public function getBudgetsAction()
+    /**
+     * @Route("/budget/add", name="create")
+     * @Method({"PUT"})
+     */
+    public function newAction()
     {
-        $data = array("Usuarios" => array(
-        array(
-            "nombre"   => "Antonio",
-            "Apellido" => "Martinez"
-        )));
-         
-        return $data;
-    }
-
-    public function postBudgetsAction()
-    {
-        $data = array("Usuarios" => array(
-        array(
-            "nombre"   => "post",
-            "Apellido" => "post"
-        )));
-         
-        return $data;
-    }
-
-    public function putBudgetsAction()
-    {
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $dm->persist($product);
+        $dm->flush();
+        return new Response( json_encode( array( 'id'=>$product->getId() ) ) );
     }
     
-    public function deleteBudgetsAction()
+    /**
+     * @Route("/budget/view/{id}", name="get")
+     * @Method({"GET"})
+     */
+    public function getAction($id)
     {
-    }  
+        $product = $this->get('doctrine_mongodb')
+            ->getRepository('AppBundle:Product')
+            ->find($id);
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id '.$id);
+        }
+        return $product;
+    }
+    /**
+     * @Route("/budget/modify", name="alter")
+     * @Method({"POST"})
+     */
+    public function alterProductAction( Request $request )
+    {
+        $id = $request->request->get('id');
+        $name = $request->request->get('name');
+        $product = $this->get('doctrine_mongodb')
+            ->getRepository('AppBundle:Product')
+            ->find($id);
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id '.$id);
+        }
+        $product->setName( $name );        
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $dm->persist($product);
+        $dm->flush();
+        die();
+    }
+    /**
+     * @Route("/budget/delete", name="delete")
+     * @Method({"DELETE"})
+     */
+    public function deleteAction( Request $request)
+    {
+        $id = $request->request->get('id');
+        
+        $product = $this->get('doctrine_mongodb')
+            ->getRepository('AppBundle:Product')
+            ->find($id);
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id '.$id);
+        }
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $dm->remove( $product );
+        $dm->flush();
+    }
 }
